a485c2fdba09f8fa31fa56fce898e391c2aafc2b
Tino Ryll <ryll@ub.uni-leipzig.de>
2019-04-25 17:27:41 +0200

    Add test data for BR1

--- mod-erm-usage-counter41/src/test/java/org/olf/erm/usage/counter41/csv/mapper/CSVMapperTest.java
+++ mod-erm-usage-counter41/src/test/java/org/olf/erm/usage/counter41/csv/mapper/CSVMapperTest.java
@@ -26,7 +26,7 @@ public class CSVMapperTest {
 
   @Parameters(name = "{0}")
   public static Collection params() {
-    return Arrays.asList("JR1", "DB1", "BR2", "PR1");
+    return Arrays.asList("JR1", "DB1", "BR2", "PR1", "BR1");
   }
 
   public CSVMapperTest(String reportName) {
