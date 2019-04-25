c30aea1e5d38f4834d4a4181f882db6edba4100a
Tino Ryll <ryll@ub.uni-leipzig.de>
2019-04-25 10:53:50 +0200

    Use parameterized test

---
+++ mod-erm-usage-counter41/src/test/java/org/olf/erm/usage/counter41/csv/mapper/CSVMapperTest.java
@@ -0,0 +1,50 @@
+package org.olf.erm.usage.counter41.csv.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.io.Resources;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collection;
+import javax.xml.bind.JAXB;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.niso.schemas.counter.Report;
+import org.niso.schemas.sushi.counter.CounterReportResponse;
+import org.olf.erm.usage.counter41.csv.CSVMapper;
+
+@RunWith(Parameterized.class)
+public class CSVMapperTest {
+
+  private String input;
+  private String expected;
+
+  @Parameters(name = "{0}")
+  public static Collection params() {
+    return Arrays.asList(
+        new Object[][] {{"JR1/NSSReport2018-01-2018-03.xml", "JR1/NSSReport2018-01-2018-03.csv"}});
+  }
+
+  public CSVMapperTest(String input, String expected) {
+    this.input = input;
+    this.expected = expected;
+  }
+
+  @Test
+  public void testToCSV() throws URISyntaxException, IOException {
+    File file = new File(Resources.getResource(input).toURI());
+    String expectedString =
+        new String(Resources.toByteArray(Resources.getResource(expected)))
+            .replace("$$$date_run$$$", LocalDate.now().toString());
+    Report report =
+        JAXB.unmarshal(file, CounterReportResponse.class).getReport().getReport().get(0);
+
+    String result = CSVMapper.toCSV(report);
+    assertThat(result).isEqualToIgnoringNewLines(expectedString);
+  }
+}
