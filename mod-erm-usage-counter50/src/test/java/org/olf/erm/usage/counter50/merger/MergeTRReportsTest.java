8616b2d27f30b11bcb6f05287488cfa1468bb5ae
alb3rtino <alb3rtino@users.noreply.github.com>
2021-06-02 14:09:57 +0200

    Add test case for merging TR_J1 report

--- mod-erm-usage-counter50/src/test/java/org/olf/erm/usage/counter50/merger/MergeTRReportsTest.java
+++ mod-erm-usage-counter50/src/test/java/org/olf/erm/usage/counter50/merger/MergeTRReportsTest.java
@@ -4,19 +4,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.olf.erm.usage.counter50.Counter5Utils;
 import org.olf.erm.usage.counter50.Counter5Utils.Counter5UtilsException;
 import org.openapitools.client.model.COUNTERTitleReport;
 import org.openapitools.client.model.COUNTERTitleUsage;
 
+@RunWith(Parameterized.class)
 public class MergeTRReportsTest extends MergerTest<COUNTERTitleReport> {
+
+  private String reportName;
+
+  public MergeTRReportsTest(String reportName) {
+    this.reportName = reportName;
+  }
+
+  @Parameters(name = "{0}")
+  public static Collection<String> params() {
+    return List.of("TR", "TRJ1");
+  }
 
   @Before
   public void setUp() {
-    prefix = "TR";
+    prefix = reportName;
   }
 
   @Test
@@ -26,14 +42,10 @@ public class MergeTRReportsTest extends MergerTest<COUNTERTitleReport> {
         Counter5Utils.merge(Arrays.asList(reports.get(0), reports.get(1), reports.get(2)));
     COUNTERTitleReport repExpected = reports.get(3);
 
-    // Need to delete reportHeader.created as assertj does not ignore nested field
-    // reportHeader.created...
-    mergedReports.getReportHeader().setCreated("");
-    repExpected.getReportHeader().setCreated("");
-
     assertThat(mergedReports.getReportHeader())
         .usingRecursiveComparison()
         .ignoringCollectionOrder()
+        .ignoringFields("created")
         .isEqualTo(repExpected.getReportHeader());
     assertThat(mergedReports.getReportItems().size())
         .isEqualTo(repExpected.getReportItems().size());
