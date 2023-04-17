3437a5e64a4f7508ec536673cb0f6ef91efcf894
alb3rtino <alb3rtino@users.noreply.github.com>
2023-04-17 23:49:31 +0200

    Extract parsing methods

--- mod-erm-usage-counter50/src/main/java/org/olf/erm/usage/counter50/csv/mapper/csv2report/CsvHeaderToReportHeader.java
+++ mod-erm-usage-counter50/src/main/java/org/olf/erm/usage/counter50/csv/mapper/csv2report/CsvHeaderToReportHeader.java
@@ -30,6 +30,79 @@ public class CsvHeaderToReportHeader {
 
   private CsvHeaderToReportHeader() {}
 
+  private static List<SUSHIOrgIdentifiers> parseOrgIdentifiers(String str) {
+    String[] splitted = str == null ? new String[0] : str.split(";");
+    return Stream.of(splitted)
+        .map(
+            s -> {
+              String[] split = s.split("=");
+              SUSHIOrgIdentifiers identifiers = new SUSHIOrgIdentifiers();
+              identifiers.setType(TypeEnum.fromValue(split[0].trim()));
+              identifiers.setValue(split[1].trim());
+              return identifiers;
+            })
+        .collect(Collectors.toList());
+  }
+
+  private static List<SUSHIReportHeaderReportFilters> parseReportHeaderReportFilters(String str) {
+    String[] splitted = str == null ? new String[0] : str.split(";");
+    return Stream.of(splitted)
+        .map(
+            s -> {
+              String[] split = s.split("=");
+              SUSHIReportHeaderReportFilters reportFilters = new SUSHIReportHeaderReportFilters();
+              reportFilters.setName(split[0].trim());
+              reportFilters.setValue(split[1].trim());
+              return reportFilters;
+            })
+        .collect(Collectors.toList());
+  }
+
+  private static List<SUSHIReportHeaderReportAttributes> parseReportHeaderReportAttributes(
+      String str) {
+    String[] splitted = str == null ? new String[0] : str.split(";");
+    return Stream.of(splitted)
+        .map(
+            s -> {
+              String[] split = s.split("=");
+              SUSHIReportHeaderReportAttributes attributes =
+                  new SUSHIReportHeaderReportAttributes();
+              attributes.setName(split[0].trim());
+              attributes.setValue(split[1].trim());
+              return attributes;
+            })
+        .collect(Collectors.toList());
+  }
+
+  private static List<SUSHIErrorModel> parseSUSHIErrorModel(String str) {
+    String[] splitted = str == null ? new String[0] : str.split(";");
+    return Stream.of(splitted)
+        .map(
+            s -> {
+              String[] split = s.split("-");
+              SUSHIErrorModel sushiErrorModel = new SUSHIErrorModel();
+              if (split.length < 3) {
+                log.error(
+                    String.format(
+                        "Exception needs to have at least 3 entries: code, severity, message. Got exception: %s",
+                        s));
+              } else {
+                sushiErrorModel.setSeverity(SeverityEnum.fromValue(split[0].trim()));
+                sushiErrorModel.setCode(Integer.valueOf(split[1].trim()));
+                sushiErrorModel.setMessage(split[2].trim());
+
+                if (split.length > 3 && !split[3].trim().equals("null")) {
+                  sushiErrorModel.setData(split[3].trim());
+                }
+                if (split.length > 4 && !split[4].trim().equals("null")) {
+                  sushiErrorModel.setHelpURL(split[4].trim());
+                }
+              }
+              return sushiErrorModel;
+            })
+        .collect(Collectors.toList());
+  }
+
   public static SUSHIReportHeader parseHeader(Map<String, String> headerColumns) {
     SUSHIReportHeader sushiReportHeader = new SUSHIReportHeader();
     sushiReportHeader.setCreated(headerColumns.get(JSON_PROPERTY_CREATED));
@@ -39,84 +112,30 @@ public class CsvHeaderToReportHeader {
     sushiReportHeader.setRelease(headerColumns.get(JSON_PROPERTY_RELEASE));
     sushiReportHeader.setInstitutionName(headerColumns.get(JSON_PROPERTY_INSTITUTION_NAME));
 
-    String instID = headerColumns.getOrDefault(JSON_PROPERTY_INSTITUTION_I_D, "");
-    String[] splittedInstID = instID == null ? new String[0] : instID.split(";");
     List<SUSHIOrgIdentifiers> orgIdentifiers =
-        Stream.of(splittedInstID)
-            .map(
-                s -> {
-                  String[] split = s.split("=");
-                  SUSHIOrgIdentifiers identifiers = new SUSHIOrgIdentifiers();
-                  identifiers.setType(TypeEnum.fromValue(split[0]));
-                  identifiers.setValue(split[1]);
-                  return identifiers;
-                })
-            .collect(Collectors.toList());
-    sushiReportHeader.setInstitutionID((orgIdentifiers.isEmpty()) ? null : orgIdentifiers);
+        parseOrgIdentifiers(headerColumns.get(JSON_PROPERTY_INSTITUTION_I_D));
+    if (!orgIdentifiers.isEmpty()) {
+      sushiReportHeader.setInstitutionID(orgIdentifiers);
+    }
 
-    String repPeriod = headerColumns.getOrDefault("Reporting_Period", "");
-    String[] splittedRepPeriod = repPeriod == null ? new String[0] : repPeriod.split(";");
-    List<SUSHIReportHeaderReportFilters> headerReportFilters =
-        Stream.of(splittedRepPeriod)
-            .map(
-                s -> {
-                  String[] split = s.split("=");
-                  SUSHIReportHeaderReportFilters reportFilters =
-                      new SUSHIReportHeaderReportFilters();
-                  reportFilters.setName(split[0].trim());
-                  reportFilters.setValue(split[1].trim());
-                  return reportFilters;
-                })
-            .collect(Collectors.toList());
-    sushiReportHeader.setReportFilters(
-        (headerReportFilters.isEmpty()) ? null : headerReportFilters);
+    List<SUSHIReportHeaderReportFilters> reportHeaderReportFilters =
+        parseReportHeaderReportFilters(headerColumns.get("Reporting_Period"));
+    if (!reportHeaderReportFilters.isEmpty()) {
+      sushiReportHeader.setReportFilters(reportHeaderReportFilters);
+    }
 
-    String reportAttrs = headerColumns.getOrDefault(JSON_PROPERTY_REPORT_ATTRIBUTES, "");
-    String[] splitted = reportAttrs == null ? new String[0] : reportAttrs.split(";");
-    List<SUSHIReportHeaderReportAttributes> headerReportAttributes =
-        Stream.of(splitted)
-            .map(
-                s -> {
-                  String[] split = s.split("=");
-                  SUSHIReportHeaderReportAttributes attributes =
-                      new SUSHIReportHeaderReportAttributes();
-                  attributes.setName(split[0].trim());
-                  attributes.setValue(split[1].trim());
-                  return attributes;
-                })
-            .collect(Collectors.toList());
-    sushiReportHeader.setReportAttributes(
-        (headerReportAttributes.isEmpty()) ? null : headerReportAttributes);
+    List<SUSHIReportHeaderReportAttributes> reportHeaderReportAttributes =
+        parseReportHeaderReportAttributes(headerColumns.get(JSON_PROPERTY_REPORT_ATTRIBUTES));
+    if (!reportHeaderReportAttributes.isEmpty()) {
+      sushiReportHeader.setReportAttributes(reportHeaderReportAttributes);
+    }
 
-    String errors = headerColumns.getOrDefault(JSON_PROPERTY_EXCEPTIONS, "");
-    String[] splittedErrors = errors == null ? new String[0] : errors.split(";");
     List<SUSHIErrorModel> errorModels =
-        Stream.of(splittedErrors)
-            .map(
-                s -> {
-                  SUSHIErrorModel sushiErrorModel = new SUSHIErrorModel();
-                  String[] split = s.split("-");
-                  if (split.length < 3) {
-                    log.error(
-                        String.format(
-                            "Exception needs to have at least 3 entries: code, severity, message. Got exception: %s",
-                            s));
-                  } else {
-                    sushiErrorModel.setSeverity(SeverityEnum.fromValue(split[0].trim()));
-                    sushiErrorModel.setCode(Integer.valueOf(split[1].trim()));
-                    sushiErrorModel.setMessage(split[2].trim());
+        parseSUSHIErrorModel(headerColumns.get(JSON_PROPERTY_EXCEPTIONS));
+    if (!errorModels.isEmpty()) {
+      sushiReportHeader.setExceptions(errorModels);
+    }
 
-                    if (split.length > 3 && !split[3].trim().equals("null")) {
-                      sushiErrorModel.setData(split[3].trim());
-                    }
-                    if (split.length > 4 && !split[4].trim().equals("null")) {
-                      sushiErrorModel.setHelpURL(split[4].trim());
-                    }
-                  }
-                  return sushiErrorModel;
-                })
-            .collect(Collectors.toList());
-    sushiReportHeader.setExceptions((errorModels.isEmpty()) ? null : errorModels);
     return sushiReportHeader;
   }
 }
