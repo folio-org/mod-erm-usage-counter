27f10ee8d47434c2500c9332bc5ac5b44629968b
alb3rtino <alb3rtino@users.noreply.github.com>
2023-04-19 12:37:56 +0200

    Fix: Handling of Publisher_ID

--- mod-erm-usage-counter50/src/main/java/org/olf/erm/usage/counter50/csv/cellprocessor/ParsePublisherID.java
+++ mod-erm-usage-counter50/src/main/java/org/olf/erm/usage/counter50/csv/cellprocessor/ParsePublisherID.java
@@ -1,8 +1,11 @@
 package org.olf.erm.usage.counter50.csv.cellprocessor;
 
-import java.util.ArrayList;
+import static java.lang.String.format;
+
+import com.google.common.base.Splitter;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.openapitools.client.model.COUNTERPublisherIdentifiers;
 import org.openapitools.client.model.COUNTERPublisherIdentifiers.TypeEnum;
 import org.supercsv.cellprocessor.CellProcessorAdaptor;
@@ -20,25 +23,34 @@ public class ParsePublisherID extends CellProcessorAdaptor {
   }
 
   private List<COUNTERPublisherIdentifiers> createPublisherIdentifiers(String input) {
-    String[] splitBySemicolon = input.split(";");
-    List<COUNTERPublisherIdentifiers> publisherIdentifiers = new ArrayList<>();
-    COUNTERPublisherIdentifiers previousPublisherIdentifier = null;
-    for (String id : splitBySemicolon) {
-      id = id.trim();
-      String[] split = id.split("=");
-      if (split.length == 1) {
-        // if length is 1 there is no type and value is appended to previous identifier's value
-        if (previousPublisherIdentifier != null) {
-          previousPublisherIdentifier.setValue(previousPublisherIdentifier.getValue() + "; " + id);
-        }
-      } else {
-        COUNTERPublisherIdentifiers cId = new COUNTERPublisherIdentifiers();
-        cId.setType(TypeEnum.fromValue(split[0]));
-        cId.setValue(split[1]);
-        publisherIdentifiers.add(cId);
-        previousPublisherIdentifier = cId;
-      }
+    return Splitter.on(";")
+        .trimResults()
+        .splitToStream(input)
+        .map(
+            identifier -> {
+              String[] split = identifier.split(":", 2);
+              if (split.length != 2) {
+                throw new PublisherIDParseException(
+                    format(
+                        "Error parsing COUNTERPublisherIdentifiers from string '%s'", identifier));
+              }
+              try {
+                return new COUNTERPublisherIdentifiers()
+                    .type(TypeEnum.fromValue(split[0]))
+                    .value(split[1]);
+              } catch (IllegalArgumentException e) {
+                return new COUNTERPublisherIdentifiers()
+                    .type(TypeEnum.PROPRIETARY)
+                    .value(identifier);
+              }
+            })
+        .collect(Collectors.toList());
+  }
+
+  static class PublisherIDParseException extends RuntimeException {
+
+    public PublisherIDParseException(String message) {
+      super(message);
     }
-    return publisherIdentifiers;
   }
 }
--- mod-erm-usage-counter50/src/main/java/org/olf/erm/usage/counter50/csv/cellprocessor/PublisherIDProcessor.java
+++ mod-erm-usage-counter50/src/main/java/org/olf/erm/usage/counter50/csv/cellprocessor/PublisherIDProcessor.java
@@ -3,6 +3,7 @@ package org.olf.erm.usage.counter50.csv.cellprocessor;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.openapitools.client.model.COUNTERPublisherIdentifiers;
+import org.openapitools.client.model.COUNTERPublisherIdentifiers.TypeEnum;
 
 public final class PublisherIDProcessor {
 
@@ -13,7 +14,14 @@ public final class PublisherIDProcessor {
       return null;
     }
     return identifiers.stream()
-        .map(id -> String.format("%s=%s", id.getType(), id.getValue()))
+        .map(
+            id -> {
+              if (TypeEnum.PROPRIETARY.equals(id.getType())) {
+                return id.getValue();
+              } else {
+                return String.join(":", id.getType().getValue(), id.getValue());
+              }
+            })
         .collect(Collectors.joining("; "));
   }
 }
